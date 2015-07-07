d30ecb4e86df70d0098b5cc40226ba9dcf16d433
Mani Monajjemi <mani.monajjemi@gmail.com>
2015-07-07 12:16:31 -0700

    Minor changes in flash script and README

--- scripts/flash.sh
+++ scripts/flash.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo dfu-programmer atmega32u4 erase && sudo dfu-programmer atmega32u4 flash ../firmware/leds_firmware.hex
+dfu-programmer atmega32u4 erase && dfu-programmer atmega32u4 flash ../../../devel/share/autonomy_leds/firmware/leds_firmware.hex
