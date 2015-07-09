65e987d0b6e240961a2535a1a33f0a079ca81225
Mani Monajjemi <mani.monajjemi@gmail.com>
2015-07-08 21:02:00 -0700

    Proof of concept low level API using ros buffer

--- autonomy_leds_avr/firmware/src/leds_firmware.cpp
+++ autonomy_leds_avr/firmware/src/leds_firmware.cpp
@@ -1,7 +1,6 @@
 /* ROS */
 #include "ros.h"
-#include "std_msgs/Empty.h"
-#include "autonomy_leds_msgs/BGRVector.h"
+#include "autonomy_leds_msgs/Command.h"
 // #include "autonomy_leds_msgs/LED.h"
 
 /* AVR */
@@ -24,7 +23,7 @@ void __cxa_pure_virtual(void) {}
 
 /* LED Memory */
 uint8_t led_counter = 0;
-autonomy_leds_msgs::BGRInt8* ros_buffer_ptr = 0;
+uint16_t* ros_buffer_ptr = 0;
 uint8_t ros_buffer_size = 0;
 
 /* Custom Write Function to LED based on apa102 */
@@ -59,7 +58,7 @@ void SPI_write(uint8_t c) {
 // State after call: SCK Low, Dat high
 }
 
-void inline apa102_setleds_ros(autonomy_leds_msgs::BGRInt8 *ledarray, uint16_t leds)
+void inline apa102_setleds_ros(uint16_t *ledarray, uint16_t leds)
 {
   uint16_t i;
   //uint8_t *rawarray=(uint8_t*)ledarray;
@@ -73,9 +72,9 @@ void inline apa102_setleds_ros(autonomy_leds_msgs::BGRInt8 *ledarray, uint16_t l
   for (i = 0; i < leds; i++)
   {
     SPI_write(0xff);  // Maximum global brightness
-    SPI_write(ledarray[i].b);
-    SPI_write(ledarray[i].g);
-    SPI_write(ledarray[i].r);
+    SPI_write(ledarray ? uint8_t((ledarray[i] & 0b0111110000000000) >> 7) : 0);
+    SPI_write(ledarray ? uint8_t((ledarray[i] & 0b0000001111100000) >> 2) : 0);
+    SPI_write(ledarray ? uint8_t((ledarray[i] & 0b0000000000011111) << 3) : 0);
   }
   
   // End frame: 8+8*(leds >> 4) clock cycles    
@@ -89,15 +88,14 @@ void inline apa102_setleds_ros(autonomy_leds_msgs::BGRInt8 *ledarray, uint16_t l
 char log_str[MAX_MSG_SIZE];
 
 /* ROS */
-void set_cb(const autonomy_leds_msgs::BGRVector& bgr_vec);
-void clear_cb(const std_msgs::Empty& msg);
+void set_cb(const autonomy_leds_msgs::Command& cmd_msg);
+// void clear_cb(const std_msgs::Empty& msg);
 // void set_led_cb(const autonomy_leds_msgs::LED& led_msg);
 // void shift_left_cb(const std_msgs::Empty& msg);
 // void shift_right_cb(const std_msgs::Empty& msg);
 
 ros::NodeHandle nh;
-ros::Subscriber<autonomy_leds_msgs::BGRVector> set_sub("leds/set", &set_cb);
-ros::Subscriber<std_msgs::Empty> clear_sub("leds/clear", &clear_cb);
+ros::Subscriber<autonomy_leds_msgs::Command> set_sub("leds/set", &set_cb);
 // ros::Subscriber<autonomy_leds_msgs::LED> set_led_sub("leds/set_led", &set_led_cb);
 //ros::Subscriber<std_msgs::Empty> shift_left_sub("leds/shift_left", &shift_left_cb);
 //ros::Subscriber<std_msgs::Empty> shift_right_sub("leds/shift_right", &shift_right_cb);
@@ -120,25 +118,27 @@ inline void ack_led()
 
 /* ROS Callbacks */
 
-void set_cb(const autonomy_leds_msgs::BGRVector& bgr_vec)
+void set_cb(const autonomy_leds_msgs::Command& cmd_msg)
 {
-  // Store the pointer to the last buffer
-  if (ros_buffer_ptr != bgr_vec.colors_vec) ack_led();
-  ros_buffer_ptr = bgr_vec.colors_vec;
-  ros_buffer_size = bgr_vec.colors_vec_length;
-  apa102_setleds_ros(ros_buffer_ptr, ros_buffer_size);
-}
-
-void clear_cb(const std_msgs::Empty& msg)
-{
-  if (ros_buffer_ptr == 0) return;
-  for (led_counter = 0; led_counter < ros_buffer_size; led_counter++)
+  ros_buffer_ptr = cmd_msg.colors_vec;
+  ros_buffer_size = cmd_msg.colors_vec_length;
+  switch (cmd_msg.flag)
   {
-      ros_buffer_ptr[led_counter].r = 0;
-      ros_buffer_ptr[led_counter].g = 0;
-      ros_buffer_ptr[led_counter].b = 0;
+    case autonomy_leds_msgs::Command::FLAG_SET_ALL:
+    {
+      apa102_setleds_ros(ros_buffer_ptr, ros_buffer_size);
+      break;
+    }
+    case autonomy_leds_msgs::Command::FLAG_CLEAR:
+    {
+      // Clear everything, it should work even w/o a buffer
+      apa102_setleds_ros(0, 255);
+      break;  
+    }
   }
-  apa102_setleds_ros(ros_buffer_ptr, ros_buffer_size);
+  
+  snprintf(log_str, MAX_MSG_SIZE, "%d", get_free_ram());
+  nh.loginfo(log_str);
 }
 
 // void set_led_cb(const autonomy_leds_msgs::LED& led_msg)
@@ -183,7 +183,6 @@ int main()
 
   nh.initNode();
   nh.subscribe(set_sub);
-  nh.subscribe(clear_sub);
   // nh.subscribe(set_led_sub);
   // nh.subscribe(shift_left_sub);
   // nh.subscribe(shift_right_sub);
@@ -205,7 +204,7 @@ int main()
   // Publish some debug information
   snprintf(log_str, MAX_MSG_SIZE, "%s", GIT_VERSION);
   nh.loginfo(log_str);
-  snprintf(log_str, MAX_MSG_SIZE, "%d %d", get_free_ram(), sizeof(autonomy_leds_msgs::BGRInt8));
+  snprintf(log_str, MAX_MSG_SIZE, "%d", get_free_ram());
   nh.loginfo(log_str);
 
   // 50hz loop
--- autonomy_leds_avr/scripts/test.py
+++ autonomy_leds_avr/scripts/test.py
@@ -1,42 +1,39 @@
 #!/usr/bin/env python
 import rospy
-from autonomy_leds_msgs.msg import BGRInt8, BGRVector, LED
+from autonomy_leds_msgs.msg import Command
 from std_msgs.msg import Empty
 
 import time
 
-num_leds = 10
+num_leds = 100
 
 def main():
-    set_pub = rospy.Publisher('leds/set', BGRVector, queue_size=10)
-    clear_pub = rospy.Publisher('leds/clear', Empty, queue_size=10)
-    set_led_pub = rospy.Publisher('leds/set_led', LED, queue_size=10)
-    sr_pub = rospy.Publisher('leds/shift_right', Empty, queue_size=10)
-    sl_pub = rospy.Publisher('leds/shift_left', Empty, queue_size=10)
+    set_pub = rospy.Publisher('leds/set', Command, queue_size=10)
     
     rospy.init_node('led_tester', anonymous = True)
     #rate = rospy.Rate(1)
     
-    empty_msg = Empty()
-    bgr_vec_msg = BGRVector()
+    command_msg = Command()
     
     rospy.loginfo("High freq test ...")
     for i in range(0, 20):
-        led = BGRInt8()
-        led.r = 0
-        led.g = 0
-        led.b = 200
-        bgr_vec_msg.colors_vec = [led] * num_leds
-        time.sleep(0.03)
-        set_pub.publish(bgr_vec_msg)
+        led = 0b0111110000000000
+        command_msg.colors_vec = [led] * num_leds
+        command_msg.flag = 0
+        time.sleep(0.2)
+        set_pub.publish(command_msg)
 
-        led.r = 0
-        led.g = 200
-        led.b = 0
-        bgr_vec_msg.colors_vec = [led] * num_leds
-        time.sleep(0.03)
-        set_pub.publish(bgr_vec_msg)
-
+        led = 0b0000000000011111
+        command_msg.colors_vec = [led] * num_leds
+        command_msg.flag = 0
+        time.sleep(0.2)
+        set_pub.publish(command_msg)
+    
+    rospy.loginfo("Clearing ...")
+    command_msg.flag = 1
+    command_msg.colors_vec = []
+    set_pub.publish(command_msg)
+    
     # rospy.loginfo("Ignore test ...")
     # bgra_vec_msg.colors_vec = []
     # for i in range(0, num_leds):
@@ -48,21 +45,21 @@ def main():
     #     bgra_vec_msg.colors_vec.append(led)
     # set_pub.publish(bgra_vec_msg)
 
-    time.sleep(0.5)
-    rospy.loginfo("Clear test ...")
-    clear_pub.publish(empty_msg)
+    # time.sleep(0.5)
+    # rospy.loginfo("Clear test ...")
+    # clear_pub.publish(empty_msg)
 
-    rospy.loginfo("Single LED test")
-    single_led = LED()
-    single_led.color.r = 255;
-    single_led.color.g = 0;
-    single_led.color.b = 0;
-    for i in range(0, num_leds):
-        clear_pub.publish(empty_msg)
-        time.sleep(0.01)
-        single_led.index = i;
-        set_led_pub.publish(single_led)
-        time.sleep(0.2);    
+    # rospy.loginfo("Single LED test")
+    # single_led = LED()
+    # single_led.color.r = 255;
+    # single_led.color.g = 0;
+    # single_led.color.b = 0;
+    # for i in range(0, num_leds):
+    #     clear_pub.publish(empty_msg)
+    #     time.sleep(0.01)
+    #     single_led.index = i;
+    #     set_led_pub.publish(single_led)
+    #     time.sleep(0.2);    
 
     # rospy.loginfo("Shift right test ...")
     # for i in range(0, num_leds):
@@ -74,8 +71,7 @@ def main():
     #     time.sleep(0.1)
     #     sl_pub.publish(empty_msg)
 
-    rospy.loginfo("Clearing ...")
-    clear_pub.publish(empty_msg)
+
 if __name__ == '__main__':
     try:
         main()
